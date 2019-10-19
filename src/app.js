9513bceb31aeee29ea82b6a0984563923903ef74
Cheena <cheena@cheena.net>
2019-10-19 14:25:00 +0000

    Use compiled assets instead of CDN

---
+++ src/app.js
@@ -0,0 +1,15 @@
+import $ from 'jquery'
+import 'popper.js'
+import 'bootstrap'
+import Typed from 'typed.js'
+
+window.jQuery = $
+window.$ = $
+
+var typed = new Typed('.typed-string', {
+  stringsElement: '#typed-strings',
+  typeSpeed: 20,
+  startDelay: 1000,
+  backDelay: 2000,
+  loop: true
+});
