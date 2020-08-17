d770a661660434d41434ab9b8802a736c762c917
Cheena <cheena@cheena.net>
2020-08-17 11:08:50 +0000

    Add deployer

---
+++ deploy.php
@@ -0,0 +1,49 @@
+<?php
+namespace Deployer;
+
+require 'recipe/common.php';
+
+// Project name
+set('application', 'cheena.net');
+
+// Project repository
+set('repository', 'git@github.com:cheenanet/cheena.net.git');
+
+// [Optional] Allocate tty for git clone. Default value is false.
+set('git_tty', true);
+
+// Shared files/dirs between deploys
+set('shared_files', []);
+set('shared_dirs', []);
+
+// Writable dirs by web server
+set('writable_dirs', []);
+set('allow_anonymous_stats', false);
+
+// Hosts
+
+host('cheena.net')
+    ->set('deploy_path', '/var/www/{{application}}');
+
+
+// Tasks
+
+desc('Deploy your project');
+task('deploy', [
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:shared',
+    'deploy:writable',
+    //'deploy:vendors',
+    'deploy:clear_paths',
+    'deploy:symlink',
+    'deploy:unlock',
+    'cleanup',
+    'success'
+]);
+
+// [Optional] If deploy fails automatically unlock.
+after('deploy:failed', 'deploy:unlock');
